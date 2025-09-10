2d5a1e9a98aff048ae961938419f45ba84cf63d2
maybeNekit <Nikson2705@gmail.com>
2025-09-10 23:28:46 +0300

    complete 3 task in contest

--- cont/contest2/Zero.cpp
+++ cont/contest2/Zero.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    int a,n,b,res;
+    cin >> a >> n;
+    b = (1 << n)-1;
+    res = a & b;
+    cout << res << endl;
     return 0;
 }
