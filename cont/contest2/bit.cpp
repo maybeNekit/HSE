2dde25b638d73a5740e9f62a23bf0b008102d3e9
maybeNekit <Nikson2705@gmail.com>
2025-09-10 22:13:52 +0300

    complete 2 task in contest

--- cont/contest2/bit.cpp
+++ cont/contest2/bit.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include <bitset>
 using namespace std;
 int main() {
-    int a;
-    cin >> a;
-    const int bits = sizeof(a);
-    bitset<bits> b(a);
-    cout << b << endl;
+    int a, b, i;
+    cin >> a>> i;
+    b =  a >> i;
+    if (b&1) {
+        cout << 1 << endl;
+    } else {
+        cout << 0 << endl;
+    }
 
     return 0;
 }
