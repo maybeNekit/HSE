1de078cb282e4f05b4aabeed518b29fe5eaa4c08
maybeNekit <Nikson2705@gmail.com>
2025-09-11 19:29:48 +0300

    complete 3 task in contest

--- seminar/main.cpp
+++ seminar/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
-using namespace std;
-
 int main() {
-    cout << "Hello, World!" << endl;
+    double a =0.1, b=0.2;
+    std::cout.setf(std::ios::fixed);
+    std::cout.precision(37);
+    std::cout<< (a+b)<<std::endl;
     return 0;
 }
+0000 0111
+0000 1100
+0000 1011
+0000 1100
+0000 0111
+
+0000 1000
+0000 1101
+0000 1000
+8
+1111 1011
+0000 1101
